30bb4fd70871cc51a107802d51cedcc9ef0bd340
Stas <stasgubarenko@yandex.ru>
2025-02-05 23:41:11 +0600

    Тест падает с ошибкой. на работе чекнуть почему

--- app/src/test/java/com/bignerdranch/android/weatherapp/data/repository/CityRepositoryImplTest.kt
+++ app/src/test/java/com/bignerdranch/android/weatherapp/data/repository/CityRepositoryImplTest.kt
@@ -1,12 +1,16 @@
 package com.bignerdranch.android.weatherapp.data.repository
 
 import com.bignerdranch.android.weatherapp.data.retrofit.CityAPI
+import com.bignerdranch.android.weatherapp.data.util.BaseAuth
 import com.bignerdranch.android.weatherapp.domain.models.city.City
 import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
+import org.mockito.ArgumentCaptor
 import org.mockito.ArgumentMatchers.anyString
+import org.mockito.ArgumentMatchers.eq
 import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
 import org.mockito.Mockito.`when`
 import retrofit2.Response
 
@@ -77,8 +81,43 @@ class CityRepositoryImplTest{
         assertEquals(expected, actual)
     }
 
+    //Согласен, название не соответствует шаблону.
+    @Test
+    fun getCities_сheckInputParametersInMethod() = runBlocking {
+        //setup
+        val mockCityAPI : CityAPI = mock()
+        val cityRepositoryImpl = CityRepositoryImpl(cityAPI = mockCityAPI)
+        val expectedResponse = Response.success(listOf<City>())
+        `when`(mockCityAPI.getCities(
+            api = eq(BaseAuth.API_CITY),
+            city = anyString(),
+            count = eq(COUNT))
+        )
+            .thenReturn(expectedResponse)
+
+        //act
+        cityRepositoryImpl.getCities(inputNameOfCity = INPUT_CITY)
+
+        //capture
+        val apiCaptor = ArgumentCaptor.forClass(String::class.java)
+        val cityCaptor = ArgumentCaptor.forClass(String::class.java)
+        val countCaptor = ArgumentCaptor.forClass(String::class.java)
+
+        //assert
+        verify(mockCityAPI).getCities(
+            api = apiCaptor.capture(),
+            city = cityCaptor.capture(),
+            count = countCaptor.capture())
+
+        assertEquals(BaseAuth.API_CITY, apiCaptor.value)
+        assertEquals(INPUT_CITY, cityCaptor.value)
+        assertEquals(COUNT, countCaptor.value)
+    }
+
     private companion object {
         private const val INPUT_CITY = "Omsk"
         private const val EMPTY_INPUT = ""
+        private const val COUNT = "5"
+        private const val API_CITY = "LkWaXCHTJcx7o4/gHxBVEQ==Q82fPDjRaKVS1lHK"
     }
 }
